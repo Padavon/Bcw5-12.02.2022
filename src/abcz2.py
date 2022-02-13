ae55b42f2eac6a13fd071891f3856ff3d40586d5
Yurii Honcharyk <gon4aryk.yurii@gmai.com>
2022-02-13 11:44:49 +0200

    multiplicity number

---
+++ src/abcz2.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    x = int(input())
+
+    if x % 2 == 0:
+        print('alpha')
+    elif x % 3 == 0:
+        print('bravo')
+    elif x % 5 == 0:
+        print('charlie')
+    else:
+        print('zulu')
